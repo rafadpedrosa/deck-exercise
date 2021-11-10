f7eebf66688723d34e277106d8e2aa137bc88e99
Rafael Pedrosa <rafael.pedrosa.br@avenuecode.com>
2021-11-10 19:05:04 -0300

    Create sort feature

--- src/views/Deck.vue
+++ src/views/Deck.vue
@@ -3,9 +3,9 @@
     <CardsContainer>
       <slot slot="body">
         <!-- maybe this can be a component-->
-        <div v-if="selectedDeck" class="card">
+        <div v-if="sortedCards" class="card">
           <div
-            v-for="(card, index) in selectedDeck.cards"
+            v-for="(card, index) in sortedCards"
             v-bind:key="index"
             class="card-img-container"
           >
@@ -13,10 +13,10 @@
           </div>
         </div>
       </slot>
-      <slot slot="footer" v-if="getRotationCardImage">
+      <slot slot="footer" v-if="rotateCard">
         <div class="rotate-card">
           <h3 class="pageTitle">Rotation Card</h3>
-          <div v-if="selectedDeck" class="card">
+          <div class="card">
             <div class="card-img-container">
               <img :src="getRotationCardImage" alt="" />
             </div>
@@ -33,10 +33,11 @@
 
 <script>
 import axios from "axios";
-import { SET_SELECTED_DECK } from "../store/mutations";
+import { SET_ALL_52_CARDS, SET_SELECTED_DECK } from "../store/mutations";
 import { mapGetters } from "vuex";
 import CardsContainer from "../components/CardsContainer";
 import SortCardsButtons from "../components/Deck/SortCardsButtons";
+import { sortSuits, sortSuitsByRotation } from "../utils/sortDecks";
 
 export default {
   components: {
@@ -46,25 +47,45 @@ export default {
   data() {
     return {
       deck_id: null,
+      sortedCards: null,
     };
   },
-
+  watch: {
+    selectedDeck() {
+      // input order is the input order of a normal deck or it is the order that I added in the new?
+      this.sortedCards = this.selectedDeck.cards;
+    },
+  },
   methods: {
     sortByInput() {
-      // do the sort
+      this.sortedCards = sortSuits(this.selectedDeck.cards);
     },
     sortByRotation() {
+      this.sortedCards = sortSuitsByRotation(
+        this.selectedDeck.cards,
+        this.all52Cards[this.rotateCard]
+      );
       // do the sort
+      console.log(":::sortByRotation");
     },
     drawCardsFromDeck() {
       // I'm getting always 10
-
       axios
         .get(
           `https://deckofcardsapi.com/api/deck/${this.deck_id}/draw?count=10`
         )
         .then(({ data }) => data)
         .then((deck) => this.$store.commit(SET_SELECTED_DECK, deck));
+    },
+    async resolveAllCardsApiProblem() {
+      // since the API is not giving me all the cards from a deck I created one to store all possible (52) cards and use it in the Create new
+      // I think you understood why I didi taht, but at the same time it might not be ok...Anyway I can explain why I did it if necessary.
+
+      const { data: allCards } = await axios.get(
+        "http://localhost:8080/allCards.json"
+      );
+
+      this.$store.commit(SET_ALL_52_CARDS, allCards);
     },
   },
   computed: {
@@ -78,6 +99,7 @@ export default {
     },
   },
   mounted() {
+    this.resolveAllCardsApiProblem();
     this.deck_id = this.$route.params.id;
     this.drawCardsFromDeck();
   },
