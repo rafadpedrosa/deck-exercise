d8bd830721105194c68e1c7ec2d53ecbc6b75ca0
Rafael Pedrosa <rafael.pedrosa.br@avenuecode.com>
2021-11-10 13:48:45 -0300

    Create SubmitDeck.vue to be used in the NewDeck.vue

---
+++ src/components/NewDeck/SubmitDeck.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="add-to-pile">
+    <div>Rotation Card</div>
+    <div>
+      <input
+        type="text"
+        maxlength="2"
+        placeholder="Card name"
+        v-model="rotation"
+      />
+      <button class="primary-color" @click="$emit('submitDeck', rotation)">
+        Submit Deck
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rotation: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  justify-items: left;
+}
+
+.add-to-pile {
+  display: flex;
+  flex-direction: column;
+}
+
+input[type="text"] {
+  border-radius: 4px;
+}
+
+button {
+  width: 150px;
+  margin-left: 25px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+}
+</style>
