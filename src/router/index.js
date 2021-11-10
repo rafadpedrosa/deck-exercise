4b1326aa6a9d6a3e9859d9c9d24e1f83733b20b1
Rafael Pedrosa <rafael.pedrosa.br@avenuecode.com>
2021-11-10 11:10:26 -0300

    Create Router for Deck and NewDeck

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Deck from "../views/Deck.vue";
+import NewDeck from "../views/NewDeck";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: "/deck/new",
+    name: "NewDeck",
+    component: NewDeck,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/deck/:id",
+    name: "Deck",
+    component: Deck,
+  },
+  {
+    path: "*",
+    name: "default",
+    redirect: "/deck/new",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "../views/NewDeck.vue"),
   },
 ];
 
---
+++ src/views/NewDeck.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="deck">
+    <h1>New Deck page</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  methods: {
+    createNewDeck() {
+      axios.get("https://deckofcardsapi.com/api/deck/new").then(console.log);
+    },
+  },
+  mounted() {
+    this.createNewDeck();
+  },
+};
+</script>
+
+<style></style>
